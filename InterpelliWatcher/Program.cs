bccd698b98ef4fc4df68a6ad90bf54824b0a0f4f
Andrea Bacigalupo <bacigalupo.andrea88@gmail.com>
2026-09-19 12:34:43 +0200

    Update Program.cs

--- InterpelliWatcher/Program.cs
+++ InterpelliWatcher/Program.cs
@@ -222,8 +222,8 @@ internal class Program
             if (!anyNewRows && allPagesAvailable)
             {
                 Log("Nessun nuovo interpello trovato.");
-                //if (config.NotifyWhenNoNews)
-                await SendTelegramMessageToAllAsync(config.TelegramBotToken, config.TelegramChatIds, "Nessun nuovo interpello trovato.");
+                if (config.NotifyWhenNoNews)
+                    await SendTelegramMessageToAllAsync(config.TelegramBotToken, config.TelegramChatIds, "Nessun nuovo interpello trovato.");
             }
             else if (!allPagesAvailable)
             {
